80be31e2f466451a2e3b3387a258e2bdd0f51f07
Phil Johnston <johnstonphilip@gmail.com>
2013-03-22 18:17:12 -0400

    Language index
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- languages/index.php
+++ languages/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Do not put custom translations here. They will be deleted on mp_stacks updates.
+ * Do not put custom translations here. They will be deleted on mp-slide updates.
  *
- * Keep custom mp_stacks translations in /wp-content/languages/mp_stacks/
+ * Keep custom mp-slide translations in /wp-content/languages/mp-slide/
  */
